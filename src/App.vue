54d5e98207f09aebb6a6d3f2b14cc15d72e2c87b
matoygwapo <101642501+matoygwapo@users.noreply.github.com>
2024-02-02 18:54:49 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import FirstComponent from './components/FirstComponent.vue';
+import {ref} from 'vue'
+
+const message = ref("This is a data from the parent component")
+
+const actionFromParent =()=>{
+  alert("Action from parent")
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hello Class</h1>
+    <FirstComponent :message="message"/>
+
+    <input type="text" v-model="message">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>THis is from first component</h2>
+        <h3>{{ props.message }}</h3>
+        <button>trigger function from parent</button>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['message'])
+
+</script>
+
+<style scoped>
+
+</style>
